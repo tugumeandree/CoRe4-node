0b3c4a9fba7160574f3b23961fbb050ee10d4a07
Tugume Andrew <andrewtugume2@gmail.com>
2020-10-28 11:13:58 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// import greet from './hello';
+const greet = require('./hello');
+const sum = require('./sum');
+
+console.log('Hello I am backend javascript AKA Nodejs');
+greet();
+ const add = sum(2,8);
+ console.log(add);
